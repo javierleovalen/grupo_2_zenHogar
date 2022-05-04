0e464a23543db897bb1e96b7ebf784f91d9205ff
javierleovalen <javier.leonardo.valencia@gmail.com>
2022-05-04 19:05:06 -0300

    agregando json

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,3 +1,5 @@
+const fs = require ('fs');
+
 const CartController = {
 
     index: (req,res) => {
@@ -9,29 +11,35 @@ const CartController = {
 
 }
 
-const items = [
+const items = JSON.parse(fs.readFileSync ('./productos.json'));
 
-    {
-
-        'id': 'silla-plegable-teca',
-        'titulo': 'Silla de jardín de madera de Teca 47x43x89cm',
-        'material': 'madera',
-        'estructura': 'Madera de Teca',
-        'esp-material': 'Un material que destaca por sus propiedades ideales para productos de exterior y especialmente por su durabilidad, resistencia y estabilidad. Una madera tropical muy popular y premium en el mobiliario de jardín.',
-        'color': 'Marron',
-        'precio': '$6000',
-        'estilo': 'Natural',
-        'dimensiones': '47 x 43 x 89cm',
-        'peso': '3 kg',
-        'caracteristicas': 'plegable',
-        'esp-tecnicas': 'Para su mantenimiento, se recomienda ubicarlo bajo cubierta, protegido del sol directo, y guardarlo en un lugar seco y ventilado cuando no lo uses (o cubrir con una funda para muebles).',
-        'montaje':'Este producto no requiere montaje',
+console.log(items[0].nombre);
 
 
 
-    },
 
-]
+
+// const items = [
+
+//     {
+
+//         'id': 'silla-plegable-teca',
+//         'titulo': 'Silla de jardín de madera de Teca 47x43x89cm',
+//         'material': 'madera',
+//         'estructura': 'Madera de Teca',
+//         'esp-material': 'Un material que destaca por sus propiedades ideales para productos de exterior y especialmente por su durabilidad, resistencia y estabilidad. Una madera tropical muy popular y premium en el mobiliario de jardín.',
+//         'color': 'Marron',
+//         'precio': '$6000',
+//         'estilo': 'Natural',
+//         'dimensiones': '47 x 43 x 89cm',
+//         'peso': '3 kg',
+//         'caracteristicas': 'plegable',
+//         'esp-tecnicas': 'Para su mantenimiento, se recomienda ubicarlo bajo cubierta, protegido del sol directo, y guardarlo en un lugar seco y ventilado cuando no lo uses (o cubrir con una funda para muebles).',
+//         'montaje':'Este producto no requiere montaje',
+
+//     },
+
+// ]
 
 
 
