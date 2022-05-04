171916017a6475240c1af3a27439b0d58589465d
javierleovalen <javier.leonardo.valencia@gmail.com>
2022-05-04 17:54:27 -0300

    agregando item principal

---
+++ controllers/CartController.js
@@ -0,0 +1,28 @@
+const items = [
+
+    {
+
+        'id': 'silla-plegable-teca',
+        'titulo': 'Silla de jardín de madera de Teca 47x43x89cm',
+        'material': 'madera',
+        'estructura': 'Madera de Teca',
+        'esp-material': 'Un material que destaca por sus propiedades ideales para productos de exterior y especialmente por su durabilidad, resistencia y estabilidad. Una madera tropical muy popular y premium en el mobiliario de jardín.',
+        'color': 'Marron',
+        'precio': '$6000',
+        'estilo': 'Natural',
+        'dimensiones': '47 x 43 x 89cm',
+        'peso': '3 kg',
+        'caracteristicas': 'plegable',
+        'esp-tecnicas': 'Para su mantenimiento, se recomienda ubicarlo bajo cubierta, protegido del sol directo, y guardarlo en un lugar seco y ventilado cuando no lo uses (o cubrir con una funda para muebles).',
+        'montaje':'Este producto no requiere montaje',
+
+
+
+    },
+
+]
+
+
+
+
+
