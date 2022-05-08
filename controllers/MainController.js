78e78230643cd82e047e23280b33a499192f6d7f
javierleovalen <javier.leonardo.valencia@gmail.com>
2022-05-08 15:48:01 -0300

    agregando mas comentarios

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,104 +1,107 @@
 const products = [
     {
-        "id":"001",
-        "name":"Sillon living comedor Verde",
+        "id": "001",
+        "name": "Sillon living comedor Verde",
         "price": 85000,
         "img": "/img/sillon_verde1.jpg",
-        "discount":40,
+        "discount": 40,
+        
     },
     {
-        "id":"002",
-        "name":"Sillon exterior de jarin",
+        "id": "002",
+        "name": "Sillon exterior de jarin",
         "price": 75000,
         "img": "/img/2019570.5.jpg",
-        "discount":30,
-
+        "discount": 30,
+        
     },
     {
-        "id":"003",
-        "name":"Silla Marron de jardin",
+        "id": "003",
+        "name": "Silla Marron de jardin",
         "price": 55000,
         "img": "/img/2019907.1.jpg",
-        "discount":20
+        "discount": 20,
+    
 
     }
 ]
 
 
 
+
 const MainController = {
 
-    home: (req,res) => {
+    home: (req, res) => {
         const styles = ['normalize', 'home']
         const titulo = 'Zen Hogar'
         res.render('home', {
-            styles:styles,
-            titulo:titulo,
-            products:products,
+            styles: styles,
+            titulo: titulo,
+            products: products,
         });
     },
 
-    login: (req,res) => {
+    login: (req, res) => {
         const styles = ['normalize', 'home', 'login'];
         const titulo = 'Zen Hogar | Login'
         res.render('login', {
-        styles:styles,
-        titulo:titulo,
+            styles: styles,
+            titulo: titulo,
         });
     },
 
-    register: (req,res) => {
-        const styles = ['normalize', 'home','register']
+    register: (req, res) => {
+        const styles = ['normalize', 'home', 'register']
         const titulo = 'Zen Hogar | Registro'
         res.render('register', {
-            styles:styles,
-            titulo:titulo,
+            styles: styles,
+            titulo: titulo,
         });
     },
 
-    create: (req,res) => {
+    create: (req, res) => {
         const styles = ['normalize', 'home', 'create']
         const titulo = 'Zen Hogar | Crear Producto'
         res.render('create', {
-            styles:styles,
-            titulo:titulo,
+            styles: styles,
+            titulo: titulo,
         });
     },
 
-    cart: (req,res) => {
-        const styles = ['normalize','home', 'cart']
+    cart: (req, res) => {
+        const styles = ['normalize', 'home', 'cart']
         const titulo = 'Zen Hogar | Mi Carrito'
         res.render('cart', {
-            styles:styles,
-            titulo:titulo,
-            products:products,
+            styles: styles,
+            titulo: titulo,
+            products: products,
         });
     },
 
-    productDetail: (req,res) => {
-        const item = products.find(item=> {
+    productDetail: (req, res) => {
+        const item = products.find(item => {
             return item.id === req.params.item;
 
         });
-        const styles = ['normalize','home', 'product-detail']
+        const styles = ['normalize', 'home', 'product-detail']
         const titulo = 'Zen Hogar | Detalle del Producto'
         res.render('product-detail', {
-            styles:styles,
-            titulo:titulo,
-            item:item,
+            styles: styles,
+            titulo: titulo,
+            item: item,
         });
     },
 
 
-    test: (req,res) => {
+    test: (req, res) => {
         const styles = ['normalize', 'home']
         const titulo = 'Titulo de prueba'
-        res.render('vista_de_prueba',{
+        res.render('vista_de_prueba', {
             styles: styles,
             titulo: titulo,
         })
     },
-   
+
 }
 
 
