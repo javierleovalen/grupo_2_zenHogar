83433dec64918d83d0726ee1e7c56a5a5f50ed6e
fgorordo <flgorordo@gmail.com>
2022-05-09 17:03:23 -0300

    identado

--- routes/index.js
+++ routes/index.js
@@ -6,39 +6,25 @@ const MainController = require ('../controllers/MainController');
 // HOME //
 router.get('/', MainController.home);
 
-
 // FORMULARIO LOGIN //
 router.get('/login', MainController.login);
-
 
 // CARRITO DE COMPRAS //
 router.get('/cart', MainController.cart);
 
-
 // HOJA DE PRUEBA //
 router.get('/test', MainController.test);
-
 
 // FORMULARIO DE REGISTO //
 router.get('/register', MainController.register);
 
-
-
 // DETALLE DE PRODUCTO //
 router.get('/productDetail/:item', MainController.productDetail);
-
 
 // CREAR PRODUCTO //
 router.get('/create', MainController.create);
 
-
 // MODIFICAR PRODUCTO //
 router.get('/modifyproduct', MainController.modifyproduct);
-
-
-
-
-
-
 
 module.exports = router;
