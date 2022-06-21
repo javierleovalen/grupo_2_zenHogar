2b88c0181713fed3092e4cd9bb29bdec097c62dd
fgorordo <flgorordo@gmail.com>
2022-06-20 21:03:07 -0300

    hotfix middleware admin

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const MainController = require ('../controllers/MainController');
 const productsRouter = require('./products')
 const usersRouter = require('./users')
 const adminRouter = require('./admin')
+const isAdminMiddleware = require('../middlewares/isAdminMiddleware')
 
 
 // HOME //
@@ -11,7 +12,7 @@ router.get('/', MainController.home);
 
 router.use('/products',productsRouter)
 router.use('/users', usersRouter)
-router.use('/admin', adminRouter)
+router.use('/admin', isAdminMiddleware,adminRouter)
 
 // 404 si la ruta no existe
 
